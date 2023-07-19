cb538a0893c6135f6036f657803ba4321269e782
David Tarditi <david.tarditi@checkedc.org>
2023-07-18 20:54:15 -0700

    Failing checks cause a SIGTRAP on ARM64.

--- tests/dynamic_checking/dynamic_check/simple-fail.c
+++ tests/dynamic_checking/dynamic_check/simple-fail.c
@@ -2,6 +2,7 @@
 //
 // The following lines are for the LLVM test harness:
 //
+// RUN %clang -###
 // RUN: %clang -Xclang -verify -o %t.exe %s %checkedc_target_flags
 // RUN: %checkedc_rununder %t.exe
 
@@ -15,9 +16,11 @@ void handle_error(int err) {
 }
 
 int main(void) {
-  // Currently the Checked C clang implementation raises a SIGILL when a
-  // dynamic check fails.  This may change in the future.
+  // Set up the handler for a failing bounds check.  Currently the Checked C
+  // clang implementation raises a SIGILL or SIGTRIP when a bounds check fails,
+  // dpending on the target OS.  This may change in the future.
   signal(SIGILL, handle_error);
+  signal(SIGTRAP, handle_error);
 
   // This is expected fail at runtime. It is simple enough for clang to issue a warning
   dynamic_check(false); // expected-warning {{dynamic check will always fail}}
